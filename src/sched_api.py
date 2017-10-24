a1c57684aad163307093f32a2c596c5e65646855
Liam Kalir <lk8150@rit.edu>
2017-10-24 16:47:00 -0400

    WIP api
    Started working on importing modles

--- src/sched_api.py
+++ src/sched_api.py
@@ -12,4 +12,39 @@ __maintainter__ = "Liam Kalir"
 __email__ = "lk8150@rit.edu"
 __status__ = "Prototype"
 
+import imp
+from pathlib import Path
 from pyomo.environ import *
+
+
+class ModelSolver(object):
+    """
+    Model solver class
+
+    Attributes:
+        modelFile (str): Path to model file
+        paramFile (str): Path to parameter file
+    """
+
+    def __init__(self, modelFile, paramFile):
+        """
+        Args:
+            modelFile (str): Path to model file
+            paramFile (str): Path to parameter file
+        """
+        super(ModelSolver, self).__init__()
+
+        # Check if files exists
+        model_file = Path(modelFile)
+        param_file = Path(paramFile)
+        if model_file.is_file() and param_file.is_file():
+            f, fname, fdesc = imp.find_module('ojModel')
+            if f is not None:
+                pack = imp.load_module('ojModel', f, fname, fdesc)
+                print(pack.model)
+
+
+if __name__ == "__main__":
+    path1 = 'C:\\Users\\Lamu\\Documents\\GitHub\\resident-scheduler\\src\\ojModel.py'
+    path2 = 'C:\\Users\\Lamu\\Documents\\GitHub\\resident-scheduler\\src\\ojData.dat'
+    mySolver = ModelSolver(path1, path2)
