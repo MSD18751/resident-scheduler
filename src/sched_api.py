c3b65ddd0e1dfc2bc00f2fd1310b2b0833f3461c
Liam Kalir <lk8150@rit.edu>
2017-10-24 15:30:41 -0400

    Added some docs

--- src/sched_api.py
+++ src/sched_api.py
@@ -1,1 +1,15 @@
+#!/usr/bin/env python
+"""
+sched.api.py: Library handling resident scheduler model
+"""
+
+__author__ = "Liam Kalir"
+__copyright__ = "Copyright 2017, Bit. Nibble. Byte."
+__credits__ = ["Liam Kalir"]
+__license__ = "GPL 3.0"
+__verision__ = "0.0.1"
+__maintainter__ = "Liam Kalir"
+__email__ = "lk8150@rit.edu"
+__status__ = "Prototype"
+
 from pyomo.environ import *
