f5f26ee2b24650902f979a43f63edce6e403eee4
Taylor Blackwell <teb5039@rit.edu>
2017-10-24 14:25:57 -0400

    Added a Dummy Model

---
+++ Trial Code.py
@@ -0,0 +1,20 @@
+# Hello World for PYOMO (Solution should be x=100)
+# This PYOMO file corresponds to the following linear program
+# mazimize z=x
+# ST
+# x<=100
+# x>=0
+
+from pyomo.environ import * #importing PYOMO modeling objects
+
+#creating the model object
+model= ConcreteModel()
+
+#declaring the decision variables
+model.x = Var(within=NonNegativeReals)
+
+#declaring the objective function
+model.maximizeZ = Objective (expr= model.x, sense=maximize)
+
+#declaring the constraints
+model.Constraint1= Constraint (expr=model.x <=100) #x can't be more than 100
