deeae2005fcf0535dc8bb57b1d85ffe4f3411374
zeuso <liam.kalir@gmail.com>
2017-10-25 23:03:54 -0400

    WIP Loading Model
    Still figuring out how to import arbitrary models

--- src/sched_api.py
+++ src/sched_api.py
@@ -3,6 +3,10 @@
 sched.api.py: Library handling resident scheduler model
 """
 
+import importlib
+from pathlib import Path
+from pyomo.environ import *
+
 __author__ = "Liam Kalir"
 __copyright__ = "Copyright 2017, Bit. Nibble. Byte."
 __credits__ = ["Liam Kalir"]
@@ -12,39 +16,57 @@ __maintainter__ = "Liam Kalir"
 __email__ = "lk8150@rit.edu"
 __status__ = "Prototype"
 
-import imp
-from pathlib import Path
-from pyomo.environ import *
-
 
 class ModelSolver(object):
     """
     Model solver class
 
     Attributes:
-        modelFile (str): Path to model file
-        paramFile (str): Path to parameter file
+        model (PyomoModel.AbstractModel): Pyomo Model
     """
 
-    def __init__(self, modelFile, paramFile):
+    def __init__(self, modelFile):
         """
         Args:
             modelFile (str): Path to model file
-            paramFile (str): Path to parameter file
         """
         super(ModelSolver, self).__init__()
 
         # Check if files exists
         model_file = Path(modelFile)
-        param_file = Path(paramFile)
-        if model_file.is_file() and param_file.is_file():
-            f, fname, fdesc = imp.find_module('ojModel')
-            if f is not None:
-                pack = imp.load_module('ojModel', f, fname, fdesc)
-                print(pack.model)
+        if model_file.is_file():
+            ldr = importlib.find_loader('ojModel')
+            if ldr is not None:
+                try:
+                    m = ldr.load_module()
+                    self.model = m.model
+                    self.optimizer = SolverFactory('glpk')
+                    self.solved = False
+                except ImportError as err:
+                    raise err
+            else:
+                raise IOError
+        else:
+            raise OSError
+
+    def LoadData(self, dataFile):
+        data_file = Path(dataFile)
+        if data_file.is_file():
+            self.data = DataPortal()
+            self.data.load(filename=dataFile, model=self.model)
+        else:
+            raise OSError
+
+    def Solve(self):
+        if not self.solved:
+            self.instance = self.model.create_instance(self.data)
+            self.optimizer.solve(self.instance)
 
 
 if __name__ == "__main__":
-    path1 = 'C:\\Users\\Lamu\\Documents\\GitHub\\resident-scheduler\\src\\ojModel.py'
-    path2 = 'C:\\Users\\Lamu\\Documents\\GitHub\\resident-scheduler\\src\\ojData.dat'
-    mySolver = ModelSolver(path1, path2)
+    path1 = 'ojModel.py'
+    path2 = 'ojData.dat'
+    mySolver = ModelSolver(path1)
+    mySolver.LoadData(path2)
+    mySolver.Solve()
+    resdict = mySolver.instance.X.get_values()
