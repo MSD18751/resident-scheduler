f33cc34f41e5262e38184b3b352659bdf1a68cb9
Daniel Fox <dcf2981@rit.edu>
2018-02-22 11:47:53 -0500

    FIxed some runtime errors. And provided a printout of sets

--- Phase-1.py
+++ Phase-1.py
@@ -2,6 +2,7 @@ import pandas as pd
 import pyomo.environ as pyomo
 from pyomo.opt import SolverFactory
 
+
 def read_excel(filename):
     """Read special Excel spreadsheet to input dict.
     
@@ -26,15 +27,15 @@ def create_model(data):
     """create pyomo abstract model"""
 
     # Make the model
-    model = pyomo.AbstractModel()    # creates an abstract model
+    model = pyomo.AbstractModel()  # creates an abstract model
     model.name = "Resident Scheduling Model"  # sets the model name 
-    model.model.data = model.data  # assigns model model.data
+    model.data = data  # assigns model model.data
 
     # Define sets
-    model.Y = pyomo.Set(initialize=model.model.data["Residents"]["Year_Level"].values.unique())  # set of resident years
-    model.R = pyomo.Set(initialize=model.model.data["Residents"].index.values)  # set of residents
-    model.U = pyomo.Set(initialize=model.model.data["Units"].index)  # set of units
-    model.Ri = pyomo.Set(initialize=model.model.data["Residents"]["Year_Level"])
+    model.Y = pyomo.Set(initialize=model.data["Residents"]["Year_Level"].unique())  # set of resident years
+    model.R = pyomo.Set(initialize=model.data["Residents"].index.values)  # set of residents
+    model.U = pyomo.Set(initialize=model.data["Units"].index)  # set of units
+    model.Ri = pyomo.Set(initialize=model.data["Residents"]["Year_Level"])
     model.Theta = pyomo.Set(initialize="MICU_D,MICU_N,Twig,OPD")  # Critical units
 
 
@@ -42,27 +43,35 @@ def create_model(data):
     # Define parameters
 
     lst = []
+
     # making a set of tuples for min and max duration
+    
     for i, j in zip(model.data["Units"]["Duration_Min"].values, model.data["Units"]["Duration_Max"].values):
         lst.append((i, j))
-    
-
     model.Lambda = pyomo.Param(model.U, within=(lst))  # number of weeks for each unit
 
     lst = []
-    for i, j, k, l, m, n in zip(
-        model.data["Units"]["R1Min"].values,
-        model.data["Units"]["R1Max"].values,
-        model.data["Units"]["R2Min"].values,
-        model.data["Units"]["R2Max"].values,
-        model.data["Units"]["R3Min"].values,
-        model.data["Units"]["R3Max"].values):
+
+    for i, j, k, l, m, n in zip(model.data["Units"]["R1Min"].values,
+                                model.data["Units"]["R1Max"].values,
+                                model.data["Units"]["R2Min"].values,
+                                model.data["Units"]["R2Max"].values,
+                                model.data["Units"]["R3Min"].values,
+                                model.data["Units"]["R3Max"].values):
         lst.append((i, j, k, l, m, n))
 
     model.Phi = pyomo.Param(model.U, within=(lst))  # min residents of year i
 
-# Solve the problem
+    # Solve the problem
 
-opt = SolverFactory("glpk")
-instance = model.create_instance()
-instance.display()
+    opt = SolverFactory("glpk")
+    instance = model.create_instance(data)
+    instance.pprint()
+
+
+def main():
+    file = "sample_data/DataFile.xlsx"
+    model_data = read_excel(file)
+    create_model(model_data)
+
+main()
