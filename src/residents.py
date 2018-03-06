d89cf04bcaf05a4414b98fc2a387ccd502a342ed
Liam Kalir <lk8150@g.rit.edu>
2018-03-06 11:17:38 -0500

    added resident model

---
+++ src/residents.py
@@ -0,0 +1,37 @@
+import pyomo.environ as pyomo
+
+# ---Define a model---
+model = pyomo.AbstractModel() # creates an abstract model
+
+# ---Define sets---
+model.R = pyomo.Set() # a set of residents
+model.U = pyomo.Set() # a set of units
+model.T = pyomo.Set() # a set of weeks
+
+# ---Define parameters---
+model.V = pyomo.Param(model.R, model.T) # the vacation preference of resident r for week t
+
+# ---Define variables---
+model.X = pyomo.Var(model.R, model.U, model.T, domain = pyomo.Binary)    # resident r does/doesn't work week t
+
+# ---Define objective function---
+def obj(model): # objective to satisfy resident vacation preferences
+    return sum(model.X[r,u,t] * model.V[r,t] for r in model.R for u in model.U for t in model.T)
+
+model.obj = pyomo.Objective(rule = obj, sense = pyomo.minimize)    # a minimization problem of the function defined above
+
+# ---Define constraints---
+def Cons1(model, t):
+    return sum(model.X[r,"A",t] for r in model.R) == 2    # make sure at least 2 residents are working unit A every week
+
+model.ResidentsA = pyomo.Constraint(model.T, rule = Cons1)    # the assignment constraint for number of residents working
+
+def Cons2(model, t):
+    return sum(model.X[r,"B",t] for r in model.R) == 2    # make sure at least 2 residents are working unit B every week
+
+model.ResidentsB = pyomo.Constraint(model.T, rule = Cons2)    # the assignment constraint for number of residents working
+
+def Cons3(model, t, r):
+    return sum(model.X[r,u,t] for u in model.U) <= 1 # makes sure that every resdient is assign to 1 place each week
+
+model.NoClones = pyomo.Constraint(model.T, model.R, rule = Cons3)
