f9ad62bdc82d7821de60568d75f640b069ceca9d
Daniel Fox <dcf2981@rit.edu>
2018-03-20 16:46:27 -0400

    Redefined model.U to be a union of the other sets. And defined Ri to make separate the residents by year.

--- Phase-1.py
+++ Phase-1.py
@@ -30,13 +30,28 @@ def create_model(data):
     # Make the model
     model = pyomo.AbstractModel()  # creates an abstract model
     model.name = "Resident Scheduling Model"  # sets the model name 
-    model.data = data  # assigns model model.data
+    model.data = data  # assigns model data
 
     # Define sets
     model.Y = pyomo.Set(initialize=model.data["Residents"]["Year_Level"].unique())  # set of resident years
     model.R = pyomo.Set(initialize=model.data["Residents"].index.values)  # set of residents
-    model.U = pyomo.Set(initialize=model.data["Units"].index)  # set of units
-    model.Ri = pyomo.Set(initialize=model.data["Residents"]["Year_Level"])
+    model.A = pyomo.Set(initialize=model.data["Units"].query("Unit_type == 'Ambulatory'").index.values)
+    model.C = pyomo.Set(initialize=model.data["Units"].query("Unit_type == 'Clinic'").index.values)
+    model.E = pyomo.Set(initialize=model.data["Units"].query("Unit_type == 'Elective'").index.values)
+    model.I = pyomo.Set(initialize=model.data["Units"].query("Unit_type == 'Inpatient'").index.values)
+    model.V = pyomo.Set(initialize=model.data["Units"].query("Unit_type == 'Vacation'").index.values)
+    model.U = model.A | model.C | model.E | model.I | model.V  # set of units
+
+    
+    Ridict = dict.fromkeys(model.data["Residents"]["Year_Level"])
+    for i in Ridict:
+        Ridict.update({i: []})
+    for i in model.data["Residents"].index.values:
+        Ridict[model.data["Residents"].iloc[i-1]["Year_Level"]].append(i)
+
+    
+    model.Ri = pyomo.Set(model.Y, initialize=Ridict)
+    model.T = pyomo.Set(initialize=range(1,53))
     model.Theta = pyomo.Set(initialize=("MICU_D", "MICU_N", "Twig", "OPD"))  # Critical units
 
 
@@ -50,10 +65,7 @@ def create_model(data):
     lambdadict = {}
     for i in data["Units"].index:
         lambdadict.update({i: int(np.mean([model.data["Units"].loc[i]["Duration_Min"], model.data["Units"].loc[i]["Duration_Max"]]))})
-    
-    print(lambdadict)
     model.Lambda = pyomo.Param(model.U, initialize=lambdadict, default=0)  # number of weeks for each unit
-    #print(model.Lambda)
 
     # p = {1: 1, 2: 4, 3: 9}
     # model.A = pyomo.Set(initialize=[1,2,3])
