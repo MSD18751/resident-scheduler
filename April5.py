3287ff56113079257ff3ee370da8b564c409402d
Taylor Blackwell <teb5039@rit.edu>
2018-04-05 12:54:34 -0400

    1254 PM

---
+++ April5.py
@@ -0,0 +1,99 @@
+import pyomo.environ as pyomo
+from pyomo.opt import SolverFactory
+import pandas as pd
+
+# ---Create a solver---
+opt = SolverFactory("glpk")
+
+# ---Define a model---
+model = pyomo.AbstractModel() # creates an abstract model
+
+# ---Define sets---
+#model.Y = pyomo.Set()   # a set of year levels
+model.R = pyomo.Set()   # a set of residents
+#model.R_i = pyomo.Set() # set of residents of year i
+model.C = pyomo.Set()   # set of clinic units
+#model.G = pyomo.Set()   # clinic rotational groups 
+model.T = pyomo.Set()   # a set of weeks
+#model.V = pyomo.Set()   # vacation unit
+model.A = pyomo.Set()   # ambulatory care units
+#model.I = pyomo.Set()   # inpatient care units
+#model.E = pyomo.Set()   # elective units
+#model.H_u = pyomo.Set() # resident year needed for unit u 
+model.U = model.C | model.A   # set of units
+#model.Q = pyomo.Set()   # units requiring a group every week
+#model.N = pyomo.Set()   # night shift rotations
+#model.theta = pyomo.Set()   # subset considered for pahse 1
+#model.S = pyomo.Set()   # standby unit
+model.P = pyomo.Set()   # a set of the types of the rotation policies
+
+
+currentpolicy = "one_one"
+policy = 1
+model_np = 4
+naught_p = policy
+
+# ---Define parameters---
+model.psi = pyomo.Param(model.R, model.T) # the vacation preference of resident r for week t
+model.naught_p = pyomo.Param(model.P) # the number of weeks between clinical rotations
+naught_p = policy
+model.s = pyomo.Param(model.P) # the number of weeks a clinical rotation lasts
+s = 1
+model.alpha_dict = {}
+model.alpha_dict[("Clinic1", "min")] = 2
+model.alpha_dict[("Clinic2", "min")] = 2
+
+# ---Define variables---
+model.X = pyomo.Var(model.R, model.U, model.T, domain = pyomo.Binary)    # resident r does/doesn't work unit u week t
+model.W = pyomo.Var(model.R, model.U, model.T, domain = pyomo.Binary)    # resident r does/doesn't START rotation unit u week t
+model.delta = pyomo.Var(model.U, model.T, domain = pyomo.Binary)    # unit u does/doesn't START a rotation in week t
+
+# ---Define objective function---
+def obj(model): # objective to satisfy resident vacation preferences
+    return sum(model.X[r,u,t] * model.psi[r,t] for r in model.R for u in model.U for t in model.T)
+
+model.obj = pyomo.Objective(rule = obj, sense = pyomo.maximize)    # a maximization problem of the function defined above
+
+# ---Define constraints---
+def Cons9(model, r, c, t):
+    for r in model.R:
+        for c in model.C:
+            for t in list(range(1, model.naught_p[currentpolicy] + model.s[currentpolicy]+1)):
+                sumofX = 0
+                q = 0
+                while t + (q * (model.naught_p[currentpolicy] + model.s[currentpolicy])) <= model_np:
+                    sumofX = sumofX + model.X[r,c,t + (q * (model.naught_p[currentpolicy] + model.s[currentpolicy]))]
+                    q = q + 1
+                #return sumofX >= model.alpha_dict[(c,"min")] * model.W[r,c,t]
+                return sumofX >= model.alpha_dict[(c,"min")]
+
+model.ClinicRotation = pyomo.Constraint(model.R, model.C, list(range(1, naught_p + s+1)), rule = Cons9)
+
+def Cons10(model, r, c):
+    for r in model.R:
+        for c in model.C:
+            sumofX = 0
+            t = 1
+            while t <= (model.naught_p[currentpolicy] + model.s[currentpolicy]):
+                sumofX = sumofX + model.X[r,c,t]
+                t = t + 1
+            return sumofX == model.s[currentpolicy]
+
+model.FirstClinic = pyomo.Constraint(model.R, model.C, rule = Cons10)
+
+def Cons14(model, t, u):
+    return 1 <= sum(model.X[r,u,t] for r in model.R) <= 3    # make sure at least 2 residents are working unit u every week
+
+model.Residents = pyomo.Constraint(model.T, model.U, rule = Cons14)    # the assignment constraint for number of residents working
+
+def Cons15(model, r, t):
+    return sum(model.X[r,u,t] for u in model.U) == 1 # makes sure that every resident is assign to max 1 place each week
+
+model.NoClones = pyomo.Constraint(model.R, model.T, rule = Cons15)
+
+
+
+# ---Create an instance---
+instance = model.create_instance("April5.dat")
+results = opt.solve(instance)
+instance.display()
